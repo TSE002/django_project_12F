5d0b01387896a7ac78ec6cfb90202645497bd727
csaba012 <papp.csaba28@gmail.com>
2021-02-18 19:48:52 +0100

    időjárás lekérdezés fix
    így már importálva is futatható lesz ahogy az kell majd nekünk

--- blog/blog_app/get_weather.py
+++ blog/blog_app/get_weather.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.idokep.hu/idojaras/Budapest"
-page = requests.get(url)
-code = BeautifulSoup(page.text, "html.parser")
-kep = code.findAll('img', attrs={'class': 'ik forecast-bigicon pr-2 pd-md-0'})[0]
-kep_link = "https://www.idokep.hu" + kep['src']
-jelenleg = code.findAll('div', attrs={'class': 'ik current-weather'})[0]
-jelenleg_szoveg = jelenleg.contents[0]
-homerseklet = code.findAll('div', attrs={'class': 'ik current-temperature'})[0]
-homerseklet = homerseklet.contents[0].rstrip()
-vissza = [kep_link, jelenleg_szoveg, homerseklet]
-return vissza
+def getData():
+	url = "https://www.idokep.hu/idojaras/Budapest"
+	page = requests.get(url)
+	code = BeautifulSoup(page.text, "html.parser")
+	kep = code.findAll('img', attrs={'class': 'ik forecast-bigicon pr-2 pd-md-0'})[0]
+	kep_link = "https://www.idokep.hu" + kep['src']
+	jelenleg = code.findAll('div', attrs={'class': 'ik current-weather'})[0]
+	jelenleg_szoveg = jelenleg.contents[0]
+	homerseklet = code.findAll('div', attrs={'class': 'ik current-temperature'})[0]
+	homerseklet = homerseklet.contents[0].rstrip()
+	vissza = [kep_link, jelenleg_szoveg, homerseklet]
+	return vissza
