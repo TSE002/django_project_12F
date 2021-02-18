542d586c2e61c552ceec688a230b0738386b7766
TSE002 <40351885+TSE002@users.noreply.github.com>
2021-02-18 13:53:34 +0100

    adatbázis konfiguráció

--- blog/blog_app/admin.py
+++ blog/blog_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Admin
+from .models import Kepek
+from .models import Cikkek
+
+admin.site.register(Admin)
+admin.site.register(Kepek)
+admin.site.register(Cikkek)
 
 # Register your models here.
--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Admin(models.Model):
+	nev = models.CharField(max_length=255)
+	felhnev = models.CharField(max_length=255)
+	psw = models.CharField(max_length=255)
+	tel = models.CharField(max_length=11)
+	fb = models.CharField(max_length=1024)
+	insta = models.CharField(max_length=1024)
+	pic_id = models.ForeignKey('Kepek',on_delete=models.CASCADE)
+
+class Cikkek(models.Model):
+	cim = models.CharField(max_length=255)
+	tartalom = models.CharField(max_length=32768)
+	datum = models.DateField()
+	felh_id = models.ForeignKey(Admin,on_delete=models.CASCADE)
+
+
+class Kepek(models.Model):
+	kep_hash = models.CharField(max_length=255)
+	cikk_id = models.ForeignKey(Cikkek,on_delete=models.CASCADE)
+
+
+
+
+
+
