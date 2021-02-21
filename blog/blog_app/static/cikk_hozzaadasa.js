d8f730f6133ad2b0270edf5a10fae97380cc8c38
csaba012 <papp.csaba28@gmail.com>
2021-02-21 15:06:32 +0100

    cikk hozzaadasa responsive template

---
+++ blog/blog_app/static/cikk_hozzaadasa.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+	let btn = document.getElementById('cikk_kep_btn');
+	let input = document.getElementById('cikk_kep');
+
+	btn.addEventListener('click', function() {
+		input.click();
+	});
+});
